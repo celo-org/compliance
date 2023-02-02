71416b1d568e870107ae8c3011f03cfe45b5b2aa
sawa <jesse.sawa@gmail.com>
2023-02-02 15:45:30 -0500

    update URL

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export async function SanctionedAddressAPI(): Promise<string[]> {
   return await axios
     .get(
-      "https://raw.githubusercontent.com/celo-org/compliance/main/ofac.sanctions.json?token=GHSAT0AAAAAAB5WK5QXU253UPDZXXMMON7YY64DONA"
+      "https://celo-org.github.io/compliance/ofac.sanctions.json"
     )
     .then((response) => {
       return response.data;
